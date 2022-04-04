78adac02c6165177352d35f2cda6d71afdf26a52
marcov55 <marcovascogit@gmail.com>
2022-04-04 17:52:43 +0000

    Correção da prova

---
+++ Fibonacci.java
@@ -0,0 +1,15 @@
+public class Fibonacci {
+    public static void main(String[] args) {
+        int sequencia[] = new int[20];
+        sequencia[0] = 0;
+        sequencia[1] = 1;
+
+        for(int indice = 2;indice < 20;indice++) {
+            sequencia[indice] = sequencia[indice-1] + sequencia[indice-2];
+        }
+        
+        for(int num : sequencia) {
+            System.out.print(num + " ");    
+        }
+    }
+}            
